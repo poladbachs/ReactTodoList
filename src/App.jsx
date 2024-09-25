c708d7974bbe2b87ab79fc7aa82510a8f589d68d
Polad <bakhip@usi.ch>
2024-09-25 15:36:16 +0200

    wrapped form and header in react fragment without extra div

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import "./styles.css"
 
 
 export default function App() {
-  return <form className="new-item-form">
-    <div className="form-row"> 
-      <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
-    </div>
-  </form>
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+    </>
+  )
 }
