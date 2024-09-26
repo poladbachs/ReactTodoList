2c36ec24dadee851a9d0a1452f2c17fcc228d935
Polad <bakhip@usi.ch>
2024-09-27 00:55:10 +0200

    TodoItem handling props

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,16 +1,16 @@
-export function TodoItem() {
+export function TodoItem({id, completed, title }) {
     return (
-        <li key={todo.id}>
+        <li>
             <label>
                 <input
                     type="checkbox"
-                    checked={todo.completed}
-                // onChange={e => toggleTodo(todo.id, e.target.checked)}
+                    checked={completed}
+                // onChange={e => toggleTodo(id, e.target.checked)}
                 />
-                {todo.title}
+                {title}
             </label>
             <button
-                // onClick={() => deleteTodo(todo.id)}
+                // onClick={() => deleteTodo(id)}
                 className="btn btn-danger">Delete
             </button>
         </li>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,12 @@
+import { TodoItem } from "./TodoItem"
+
 export function TodoList({ todos }) {
     return (
         <ul className="list">
             {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
                 return (
-                    
+                    <TodoItem />
                 )
             })}
         </ul>
